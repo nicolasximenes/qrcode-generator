32c0f2cd8c14c525df78efe097b9582d0628c156
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-02-28 00:30:16 -0300

    start of project with basic functionalities

---
+++ README.md
@@ -0,0 +1,7 @@
+# Basic QR Code Generator App
+
+This app will generate link you paste in.
+
+## About this project
+
+This project is only for studies using React JS and QRCode.
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,65 @@
+import QRCode from 'qrcode'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [url, setUrl] = useState('')
+  const [qrcode, setQRcode] = useState('')
+
+  const generateQRcode = (e) => {
+    e.preventDefault()
+
+    QRCode.toDataURL(
+      url,
+      {
+        width: 300,
+        margin: 1,
+      },
+      (err, res) => {
+        if (err) return console.error(err)
+        setQRcode(res)
+    })
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      
+      <h1 className="app-name">QR Code Generator by NX Dev</h1>
+      <p className="app-desc">This app will generate link you paste in</p>
+      
+      <form
+        className="form"
+        onSubmit={generateQRcode}
+      >
+        <input
+          type="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="input-field"
+          placeholder="paste your link"
+        />
+        <button
+          type="submit"
+          className="btn-gen"
+        >
+          Generate
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
+      
+      {qrcode && (
+        <div className="qrcode-container">
+          <img src={qrcode} alt="qrcode-img" />
+          <a
+            href={qrcode}
+            download="qrcode"
+            className="btn-download"
+          >
+            <span>Download</span>
+          </a>
+        </div>
+      )}
+
     </div>
   )
 }
